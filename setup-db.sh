f1f089a2f36bc0ba862eb9395e3a2a75e572988d
Vikrom Narula <vikrom.nar@gmail.com>
2022-04-05 13:53:42 +0700

    Better configs

--- setup-db.sh
+++ setup-db.sh
@@ -2,9 +2,13 @@
 
 warehouse=10
 
+BGREEN='\033[1;32m'
+
+echo -e "${BGREEN}----------------> ${BGREEN}Copy Config"
 cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 
+echo -e "${BGREEN}----------------> ${BGREEN}Edit Bashrc"
 echo '' >> ~/.bashrc
 echo 'ip="192.168.122."' >> ~/.bashrc
 echo 'dbip="192.168.122."' >> ~/.bashrc
@@ -20,23 +24,24 @@ echo 'alias ee="vim ."' >> ~/.bashrc
 echo 'alias e="vim"' >> ~/.bashrc
 echo 'alias ss="source ~/.bashrc"' >> ~/.bashrc
 
+echo -e "${BGREEN}----------------> ${BGREEN}Install Postgres"
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential -y postgresql-client-common postgresql libpq-dev -y
 sudo systemctl enable ssh --now
 
+echo -e "${BGREEN}----------------> ${BGREEN}Config Postgres"
 sudo rm /etc/postgresql/12/main/pg_hba.conf
 sudo cp postgres-config.conf /etc/postgresql/12/main/pg_hba.conf
 sudo bash -c "echo \"listen_addresses = '*'\" >> /etc/postgresql/12/main/postgresql.conf"
 
-sudo pg_ctlcluster 12 main start
-
-cd create-data
-bash create-data.sh $warehouse
-
+echo -e "${BGREEN}----------------> ${BGREEN}Start Postgres"
 sudo pg_ctlcluster 12 main start
 sudo service postgresql restart
 
+echo -e "${BGREEN}----------------> ${BGREEN}Gen Data"
+cd create-data
+bash create-data.sh $warehouse
+
+echo -e "${BGREEN}----------------> ${BGREEN}Gen DB"
 cd ..
 bash gen-db.sh
-
-source ~/.bashrc
--- setup-vm.sh
+++ setup-vm.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+BGREEN='\033[1;32m'
+
+echo -e "${BGREEN}----------------> ${BGREEN}Copy Config"
 cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 
+echo -e "${BGREEN}----------------> ${BGREEN}Edit Bashrc"
 echo '' >> ~/.bashrc
 echo 'ip="192.168.122."' >> ~/.bashrc
 echo 'dbip="192.168.122."' >> ~/.bashrc
@@ -18,12 +22,13 @@ echo 'alias ee="vim ."' >> ~/.bashrc
 echo 'alias e="vim"' >> ~/.bashrc
 echo 'alias ss="source ~/.bashrc"' >> ~/.bashrc
 
+echo -e "${BGREEN}----------------> ${BGREEN}Install Reqs"
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential libpq-dev -y
 sudo systemctl enable docker.service --now
 sudo systemctl enable ssh --now
 
-# Install Docker compose
+echo -e "${BGREEN}----------------> ${BGREEN}Install Docker Compose"
 # Get latest docker compose released tag
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
@@ -31,12 +36,12 @@ COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/l
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 
+echo -e "${BGREEN}----------------> ${BGREEN}Mange Docker Permission"
 sudo chown $USER /var/run/docker.sock
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
+echo -e "${BGREEN}----------------> ${BGREEN}Install API Reqs"
 cd server
 bash get-req.sh
-
-source ~/.bashrc
