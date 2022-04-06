cfb9e60fdad4c7ae80514845c53ace2065c5878f
Vikrom Narula <vikrom.nar@gmail.com>
2022-04-06 13:15:32 +0700

    Change logging

--- setup-db.sh
+++ setup-db.sh
@@ -33,7 +33,7 @@ echo -e "${BGREEN}----------------> ${BGREEN}Config Postgres"
 sudo rm /etc/postgresql/12/main/pg_hba.conf
 sudo cp postgres-config.conf /etc/postgresql/12/main/pg_hba.conf
 sudo bash -c "echo \"listen_addresses = '*'\" >> /etc/postgresql/12/main/postgresql.conf"
-sudo bash -c "echo \"log_statement = 'all'\" >> /etc/postgresql/12/main/postgresql.conf"
+sudo bash -c "echo \"log_statement = 'ddl'\" >> /etc/postgresql/12/main/postgresql.conf"
 sudo bash -c "echo \"max_wal_size = 5GB\" >> /etc/postgresql/12/main/postgresql.conf"
 
 echo -e "${BGREEN}----------------> ${BGREEN}Start Postgres"
