ffdd6cd5101b0e2002a98eefdcc1b817835cae6c
Vikrom Narula <vikrom.nar@gmail.com>
2022-04-13 14:17:34 +0700

    Change DB vm size

--- vm-scripts/gen-api-vm.sh
+++ vm-scripts/gen-api-vm.sh
@@ -4,6 +4,6 @@ virt-install \
   --name ubuntu \
   --vcpus 4 \
   --ram 8192 \
-  --disk path=u20.qcow2,size=45 \
+  --disk path=u20.qcow2,size=15 \
   --console pty,target_type=serial \
   --cdrom ubuntu-20.04.4-live-server-amd64.iso
--- vm-scripts/gen-db-vm.sh
+++ vm-scripts/gen-db-vm.sh
@@ -4,6 +4,6 @@ virt-install \
   --name database \
   --vcpus 4 \
   --ram 8192 \
-  --disk path=db.qcow2,size=45 \
+  --disk path=db.qcow2,size=85 \
   --console pty,target_type=serial \
   --cdrom ubuntu-20.04.4-live-server-amd64.iso
